8392bf0e5c9d8740be8c4ef23145d36656064207
AliBi <gublin.97@gmail.com>
2025-07-16 16:26:19 +0330

    Move mvn-push.gradle.kts to StickyLayoutManager module

--- StickyLayoutManager/build.gradle.kts
+++ StickyLayoutManager/build.gradle.kts
@@ -1,7 +1,10 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
+    id("maven-publish")
 }
 
 android {
@@ -36,4 +39,34 @@ android {
 dependencies {
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("androidx.core:core-ktx:1.16.0")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.gitlab.hooshkar"
+                artifactId = "sticky-layout-manager"
+                version = "1.0.0"
+            }
+        }
+
+        repositories {
+            maven {
+                val projectId = gradleLocalProperties(rootDir, providers).getProperty("projectId")
+                val gitLabPrivateToken = gradleLocalProperties(rootDir, providers).getProperty("gitLabPrivateToken")
+
+                url = uri("https://gitlab.hooshkar.com/api/v4/projects/$projectId/packages/maven")
+                name = "Maven"
+                credentials(HttpHeaderCredentials::class) {
+                    name = "Deploy-Token" // this isn't token name!
+                    value = gitLabPrivateToken
+                }
+                authentication {
+                    create<HttpHeaderAuthentication>("header")
+                }
+            }
+        }
+    }
 }
